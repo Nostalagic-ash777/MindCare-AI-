d479c164aaf46c8782e0244df058eb675e9e48f7
Nostalagic-ash777 <nandhanash17@gmail.com>
2025-08-29 22:40:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -420,6 +420,8 @@ def internal_error(error):
     db.session.rollback()
     return render_template("500.html"), 500
 
+with app.app_context():
+    db.create_all()
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
